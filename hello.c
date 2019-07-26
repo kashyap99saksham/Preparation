dc35a31c6ef782711426d01eb043d77fea45ed95
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2019-07-26 23:08:19 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -18,7 +18,7 @@ int main()
     char s[100];
     gets(s);
 // 	OR
-//   scanf("%[^\n]%*c", s);
+//   scanf("%[^\n]%*c", s);     --->Here, [] is the scanset character. ^\n stands for taking input until a newline isn't encountered. Then, with this %*c, it reads the newline character and here, the used * indicates that this newline character is discarded.
     printf("Hello, World!\n%s", s);
     
     return 0;
