edf9cd0217f8477c01a1df29c744e024c6477c56
Saksham Kashyap <47538539+kashyap99saksham@users.noreply.github.com>
2019-08-10 11:18:59 +0530

    Update extern.c

--- Quick_revision_of_full_c/extern.c
+++ Quick_revision_of_full_c/extern.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-extern int a;
+extern int a;   //HERE  A IS ONLY DECLARED NOT DEFINED
 int main()
 {   
-    int a = 20;
+    int a = 20; //HERE IT IS DEFINED
     printf("%d",a);
 }
+//SO EXTERN IS USED TO SEPERATE DECLARED AND DEFINING VARIABLE
+
+
+/*IN CASE OF FUNCTION
+int foo(int arg1, char arg2);   IT IS OLNY DECLARED NOT DEFINED
+ 
+ There’s an extern present at the beginning which is hidden and the compiler treats it as below.
+
+extern int foo(int arg1, char arg2);
+*/
